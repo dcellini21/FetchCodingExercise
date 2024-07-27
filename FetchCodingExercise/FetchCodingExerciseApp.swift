cdf026264e2f44cd407c9181558ad1fd371665b5
Dominic Cellini <dominic.cellini@gmail.com>
2024-07-27 14:43:11 -0400

    Initial Commit

---
+++ FetchCodingExercise/FetchCodingExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingExerciseApp.swift
+//  FetchCodingExercise
+//
+//  Created by Dom on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchCodingExerciseTests/FetchCodingExerciseTests.swift
@@ -0,0 +1,16 @@
+//
+//  FetchCodingExerciseTests.swift
+//  FetchCodingExerciseTests
+//
+//  Created by Dom on 7/27/24.
+//
+
+import Testing
+
+struct FetchCodingExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
